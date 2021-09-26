458f74728b6ac1ffe097a16b4d4b26ca407cdc18
Saim Rizvi <shr2mz@gmail.com>
2021-09-26 19:39:43 -0400

    commit tree 798c8d50a7dc0d1469b63371387b83188c20eae0

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    const text = document.getElementById('text')
+    text.textContent = 'This is really cool!'
+})
+
+console.log('hello')
